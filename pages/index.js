31ef626474c5c70f2e87f1a002e52bf0455a5ada
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2024-04-05 12:00:21 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
+  console.log(props.jokeData)
   return (
     <>
       <Head>
@@ -15,28 +17,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+        <div >
+          <h1>MongoDB: {props.mongodb}</h1>
+          <h1>APP NAME: {props.name}</h1>
+          <h1>APP EMAIL: {props.email}</h1>
+          <h1>APP PASSWORD: {props.password}</h1>
+
         </div>
 
         <div className={styles.center}>
@@ -50,65 +36,44 @@ export default function Home() {
           />
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
     </>
   );
 }
+
+// pages/index.js
+
+export async function getServerSideProps(context) {
+  // Fetch data from an API or perform any other asynchronous tasks
+  const mongodb = process.env.NEXT_APP_MONGODB;
+  const name = process.env.NEXT_APP_NAME;
+  const email = process.env.NEXT_APP_EMAIL;
+  const password = process.env.NEXT_APP_PWD;
+  const jokeUrl = process.env.NEXT_APP_JOKE_URL;
+
+  console.log("MongoDB:", mongodb);
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Password:", password);
+  console.log("Joke URL:", jokeUrl);
+
+  const data = await fetch(jokeUrl)
+
+  const jsonData = await data.json();
+
+  console.log(jsonData)
+
+  // Return the data as props
+  return {
+    props: {
+      mongodb,
+      name,
+      email,
+      password,
+      jokeData:JSON.parse(JSON.stringify(jsonData))
+    }
+  };
+}
+
+
